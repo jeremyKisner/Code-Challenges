e263cc5bcb08415c6a8c0850d938f34160447900
Jeremy Kisner <jeremy.kisner@move.com>
2019-05-13 21:54:19 -0400

    Adding challenge

--- code-challenges/src/challenges/MatrixElementsSum.java
+++ code-challenges/src/challenges/MatrixElementsSum.java
@@ -3,6 +3,12 @@ package challenges;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+/*
+ * Given matrix, a rectangular matrix of integers, where each value represents the cost of the room, 
+ * your task is to return the total sum of all rooms that are suitable for the CodeBots 
+ * (ie: add up all the values that don't appear below a 0).
+ */
+
 public class MatrixElementsSum {
 
 	public static void main(String[] args) {
