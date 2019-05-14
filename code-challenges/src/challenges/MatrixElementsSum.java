aa2cbb076962dc7bcc5b3d979e0559e2bdb9ccab
Jeremy Kisner <jeremy.kisner@move.com>
2019-05-13 21:53:37 -0400

    Adding challenge

---
+++ code-challenges/src/challenges/MatrixElementsSum.java
@@ -0,0 +1,29 @@
+package challenges;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class MatrixElementsSum {
+
+	public static void main(String[] args) {
+		int[][] matrix = {{1, 1, 1, 0},
+		                  {0, 5, 0, 1}, 
+		                  {2, 1, 3, 10}};
+		matrixElementsSum(matrix);
+	}
+	
+	static int matrixElementsSum(int[][] matrix) {
+		int roomPriceSum = 0;
+		Set<Integer> columnsToSkip = new LinkedHashSet<Integer>();
+		for (int row = 0; row < matrix.length; row++) {
+			for (int column = 0; column < matrix[row].length; column++) {
+				if (columnsToSkip.contains(column)) continue;
+				if (matrix[row][column] == 0) columnsToSkip.add(column);
+				System.out.println(matrix[row][column]);
+				roomPriceSum += matrix[row][column];
+			}
+		}
+		System.out.println(roomPriceSum);
+		return roomPriceSum;
+	}
+}
